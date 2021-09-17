2d2cb0463182d695086d05f2fdf48de3cb44f253
MarkZither <mark.burton@zither-it.co.uk>
2021-09-17 23:33:12 +0200

    Add template projects

---
+++ src/MarkZither.KimaiDotNet.OutlookAddin/CalendarRibbon.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MarkZither.KimaiDotNet.OutlookAddin
+{
+    public partial class CalendarRibbon
+    {
+        private void CalendarRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+    }
+}
